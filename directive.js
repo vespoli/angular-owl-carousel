5d78ec5f3bd86cda797c589a0454efe6bb7aec3a
David Vespoli <david.vespoli@gmail.com>
2015-07-23 11:55:23 -0700

    adding example with events

--- directive.js
+++ directive.js
@@ -29,3 +29,30 @@ angular.module('ccbApp').directive('owlCarouselItem', [function() {
     }
   };
 }]);
+
+//example with events attached
+// angular.module('ccbApp').directive('owlGalleryCarousel', function(){
+//   return {
+//     restrict: 'A',
+//     transclude: false,
+//     link: function (scope,element) {
+              
+//       element.on('initialized.owl.carousel changed.owl.carousel', function(event) {
+//         scope.itemNumber = event.item.index+1;
+//         scope.itemCount = event.item.count;
+//         document.getElementById("item-number").innerHTML = "" + scope.itemNumber + "";
+//       })
+
+//       scope.initCarousel = function(element) {
+//         var defaultOptions = {};
+//         var customOptions = scope.$eval($(element).attr('data-options'));
+//         // combine the two options objects
+//         for(var key in customOptions) {
+//           defaultOptions[key] = customOptions[key];
+//         }
+//         // init carousel
+//         $(element).owlCarousel(defaultOptions);                
+//       };
+//     }
+//   };
+// });
